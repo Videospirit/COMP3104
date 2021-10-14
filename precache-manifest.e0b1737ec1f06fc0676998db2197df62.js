9578e4f4021c8cd86c2a485985ba2a98b8a938e1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-14 00:11:19 +0000

    Deploy Videospirit/COMP3104 to github.com/Videospirit/COMP3104.git:gh-pages

---
+++ precache-manifest.e0b1737ec1f06fc0676998db2197df62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c0b2a5dc9c587470ef8",
+    "url": "/Videospirit/COMP3104/static/css/main.66556d64.chunk.css"
+  },
+  {
+    "revision": "6c0b2a5dc9c587470ef8",
+    "url": "/Videospirit/COMP3104/static/js/main.6c0b2a5d.chunk.js"
+  },
+  {
+    "revision": "4faf9ffe7ce6dbbd5110",
+    "url": "/Videospirit/COMP3104/static/js/runtime~main.4faf9ffe.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Videospirit/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "1229f98d14b4e89a53e73cb85611ed79",
+    "url": "/Videospirit/COMP3104/index.html"
+  }
+];
